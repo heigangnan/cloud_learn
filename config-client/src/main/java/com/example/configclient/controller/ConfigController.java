44dc9b10c0825bf96c557e0c842cd90d0e2debfd
lixing <lx40226@ly.com>
2018-07-18 17:51:56 +0800

    消息总线bus+rabbitmq

--- config-client/src/main/java/com/example/configclient/controller/ConfigController.java
+++ config-client/src/main/java/com/example/configclient/controller/ConfigController.java
@@ -1,11 +1,21 @@
 package com.example.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigController {
+
+    /**
+     * 配置文件更新后，发送post请求下面的地址，可以动态更新，重新拉取配置（注意：是post请求）
+     * http://localhost:8881/actuator/bus-refresh
+     * /actuator/bus-refresh接口可以指定服务，
+     * 即使用”destination”参数，比如 “/actuator/bus-refresh?destination=customers:**” 即刷新服务名为customers的所有服务。
+     * 调用没有反应：查看pom依赖是否正确；controller上是否加了@RefreshScope；是否是post请求
+     */
 
     // 从config-server加载的配置文件
     @Value("${foo}")
